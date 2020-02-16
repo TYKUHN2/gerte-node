2fd36d3731db362c3f83a1435a737a7ef1643af9
Tyler Kuhn <contact@tyknet.tk>
2020-02-15 22:22:14 -0500

    Added Version Export
    Added new version export for the API version.

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 	Exported variables:
 	
 	version:	GERTe protocol version (byte encoded)
+	api:		API version (byte encoded)
 	Connection:	Connection class for connecting to GERTe
 	Config:		Config function that returns a array of peers
 */
 
 exports["version"] = "\1\1";
+exports["api"] = "\1\1\1";
 exports["Connection"] = require("./Classes/Connection.js");
 exports["Config"] = require("./Classes/Config.js");
