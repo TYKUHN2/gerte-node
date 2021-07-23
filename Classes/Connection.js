d8a53080f354766e1ee3361c018b20861ea4ba2d
Tyler Kuhn <contact@tyknet.tk>
2021-07-23 00:45:43 -0400

    Updated to GERTe 2.0
    Updated classes to GERTe 2.0 compliance.

--- Classes/Connection.js
+++ Classes/Connection.js
@@ -1,280 +1,188 @@
+'use strict';
+
 const net = require("net");
-const Event = require("events");
-const Version = require("../main.js").version;
+const crypto = require("crypto");
+const Identity = require("./Identity.js");
+const EventEmitter = require("events");
+const Address = require("./Address.js");
 const Packet = require("./Packet.js");
-const Status = require("./Status.js");
+const KeyDatabase = require("./KeyDatabase.js");
 
-function convAddr(addr) {
-	let parts = addr.split(".");
-	let buf = Buffer.alloc(3);
-	
-	parts[0] = parseInt(parts[0], 10);
-	parts[1] = parseInt(parts[1], 10);
-	
-	buf[0] = parts[0] >> 4;
-	buf[1] = ((parts[0] << 4) & 0xF0) | parts[1] >> 8;
-	buf[2] = parts[1] & 0xFF;
-	
-	return buf;
-}
+/**
+ * Represents a connection to a peer and handles negotiation and communication.
+ */
+class Connection extends EventEmitter {
+    /**
+     * @type {net.Socket}
+     */
+    #socket;
 
-function ondata(data) {
-	if (this._buffer.length > 0) {
-		data = Buffer.concat([this._buffer, data], this._buffer.length + data.length);
-	}
-	
-	while (data.length > 0) {
-		switch(data[0]) {
-			case 0x00:
-				let status;
-			
-				try {
-					status = Status(data.slice(1));
-					
-					this.emit("status", status);
-					data = data.slice(1 + status.size);
-					break;
-				} catch (e) {
-                    this._buffer = data;
-					
-					if (e.message != "data") {
-						throw e;
-					} else {
-						return;
-					}
-				}
-			case 0x01:
-				throw new Error("GEDS returned an invalid command [REGISTER]");
-			case 0x02:
-				let packet;
-				
-				try {
-					packet = Packet(data.slice(1));
-					
-					this.emit("data", packet);
-					data = data.slice(14 + packet.data.length);
-					break;
-				} catch (e) {
-                    this._buffer = data;
-					
-					if (e.message != "data") {
-						throw e;
-					} else {
-						return;
-					}
-				}
-			case 0x03:
-				this.emit("close");
-				data = data.slice(1);
-		}
-    }
+    /**
+     * @type {Identity}
+     */
+    #identity;
 
-    this._buffer = new Buffer.alloc(0);
-}
+    #ready = false;
 
-/*
-	EVENTS:
-	
-	error:		(Error error)		Connection error occurred during negotiation.
-	connected:	()					Connection has finished negotiating.
-	status:		(Status status)		GEDS has sent a new STATUS message. Connection will internally handle this.
-	registered:	(String address)	GEDS has accepted the address-key pair and data can now be sent.	NOTE: If address is "" then it is unknown
-	data:		(Packet data)		GEDS has sent a new data packet.
-	close:		()					The connection has been closed through one of many means.
-	
-	FUNCTIONS:
-	
-	send:		(String target, String source, String/Buffer data)	(Promise)	Send data to a specified GERTc target from the GERTi source. Data cannot be bigger than 255 bytes. Returns a promise for the result which will be null on success and the error code on failure (resolve vs reject.)
-	register:	(String address, String key)                        ()     		Register the GERTe address using the 20 byte key. Does not return, listen for the registered event.
-    close:      ()                                                  ()          Safely closes the connection.
-	
-	VARIABLES:
-	address:	The currently registered GERTe address or an empty string if not registered. Can be "unknown".
-*/
+    /**
+     * @type {[Packet]}
+     */
+    #pending = [];
 
-function onSocketClose() {
-	this.emit("close");
-};
+    /**
+     * @type {[Packet]}
+     */
+    #packets = [];
 
-function onClose() {
-	this._socket.off("close", onSocketClose);
-	this._socket.destroy();
-}
-
-function onConnected(status) {
-	if (status.state == "connected") {
-		this.emit("connected");
-		this.on("status", onStatus);
-	} else {
-		this.emit("error", new Error("GEDS did not accept the connection."));
-	}
-}
-
-function onRegistered(status, newAddr) {
-	if (status.state == "assigned") {
-		this._addr = newAddr;
-		this.emit("registered", newAddr);
-		return;
-	} else if (status.state == "failure") {
-		if (status.error == "already registered") {
-			this.emit("error", new Error("GEDS reported a double registration"));
-			this._addr = "unknown";
-			this.emit("registered", "")
-			return;
-		} else if (status.error == "bad key") {
-			this.emit("error", new Error("GEDS rejected the supplied key"));
-			return;
-		} else if (status.error == "address taken") {
-			this.emit("error", new Error("GEDS reports that the requested address is already claimed"));
-			return;
-		}
-	} else {
-		this.once("status", (status) => {
-			onRegistered.call(this, status, newAddr);
-		});
-	}
-}
-
-function onStatus(status) {
-    if (status.state == "failure") {
-        if (status.error == "not registered") {
-            this._addr = "";
-            this.emit("error", "GEDS lost the registration");
-            this._outstanding.pop().reject("not registered");
-        } else if (status.error == "no route") {
-            this._outstanding.pop().reject("no route");
-        }
-    } else if (status.state == "sent") {
-        this._outstanding.pop().resolve();
-    } else if (status.state == "closed") {
-        this.emit("close");
-    }
-}
-
-class Connection extends Event {
-    constructor(port, ip) {
+    /**
+     * Connects to the given peer using a given identity.
+     *
+     * @param {string} ip
+     * @param {number} port
+     * @param {Identity} identity
+     */
+    constructor(ip, port, identity) {
         super();
 
-		let _this = this;
-		
-        let socket = net.connect(port, ip, () => {
-			socket.on("data", (data) => {
-				ondata.call(_this, data);
-			});
-			
-			this.once("status", onConnected);
-			
-			this.on("close", onClose);
-			
-			socket.on("close", () => {
-				onSocketClose.call(_this);
+        if (!net.isIP(ip))
+            throw RangeError("Connection ip is not a valid IP address");
+        else if (typeof port !== "number")
+            throw TypeError("Connection port is not an integer");
+        else if (port > 65535)
+            throw RangeError("Connection port is above 65535");
+        else if (!identity instanceof Identity)
+            throw TypeError("Connection identity is not an Identity");
+
+        this.#identity = identity;
+        this.#socket = net.createConnection(port, ip, () => {
+            let packet = Buffer.from([2, 0]);
+            packet = Buffer.concat([packet, identity.address.buffer], 5);
+
+            let fthis = this;
+
+            this.#socket.on("close", () => { fthis.emit("close"); });
+
+            this.#socket.once("data", (data) => {
+                fthis.#handshake(data)
             });
-
-            socket.write(Version);
-            _this._connected = true;
-		});
-		
-        socket.on("error", (e) => {
-            if (e.code == "ECONNRESET" || e.code == "ECONNREFUSED" || e.code == "ETIMEDOUT" ) {
-                return;
-            }
-
-			_this.emit("error", e);
-			this.destroy();
-		});
-		
-		this._socket = socket;
-		this._addr = "";
-        this._buffer = new Buffer.alloc(0);
-        this._outstanding = [];
-        this._connected = false;
-	}
-	
-	send(tgt, src, data) {
-		if (this._socket.destroyed) {
-			throw new Error("Connection has been destroyed");
-		} else if (this._addr == "") {
-			throw new Error("Connection has not been registered");
-		}
-		
-		if (typeof(data) == "string") {
-			data = Buffer.from(data);
-		}
-		
-		if (data.length > 255) {
-			throw new Error("Data cannot exceed 255 bytes");
-		}
-		
-		tgt = tgt.split(":");
-		let tgte = convAddr(tgt[0]);
-		let tgti = convAddr(tgt[1]);
-		src = convAddr(src);
-		
-		let packet = Buffer.alloc(11 + data.length);
-
-        packet[0] = 0x02;
-		tgte.copy(packet, 1);
-		tgti.copy(packet, 4);
-		src.copy(packet, 7);
-		packet[10] = data.length;
-		data.copy(packet, 11);
-		
-		this._socket.write(packet);
-		
-		let promise = new Promise((resolve, reject) => {
-			this._outstanding.push({
-				"resolve": resolve,
-				"reject": reject
-			});
-		});
-		
-		return promise;
-	}
-	
-	register(newAddr, key) {
-		if (this._socket.destroyed) {
-			throw new Error("Connection has been destroyed");
-		} else if (this._addr != "") {
-			throw new Error("Connection has already been given an address");
-		} else if (this._addr == "unknown") {
-			console.warn("Connection is registered but address is unknown, assuming the address set was intended to fix this");
-			this._addr = newAddr;
-			return;
-		} else if (key.length != 20) {
-			throw new Error("Provided key is not the correct length");
-		}
-		
-		let data = convAddr(newAddr);
-		
-		let packet = Buffer.alloc(24);
-		packet[0] = 0x01;
-		data.copy(packet, 1);
-        packet.write(key, 4, 20);
-
-		if (!this._connected) {
-			this.on("connected", () => {
-				this._socket.write(packet);
-				
-				this.once("status", (status) => {
-					onRegistered.call(this, status, newAddr);
-				});
-			});
-        } else {
-            this._socket.write(packet);
-
-			this.once("status", (status) => {
-				onRegistered.call(this, status, newAddr);
-			});
-		}
-	}
-	
-	get address() {
-		return this._addr || "none";
+            this.#socket.write(packet);
+        });
     }
 
-    close() {
-        this._socket.write(0x03);
-        this._socket.end();
+    /**
+     * Sends a Packet
+     * @param {Packet} packet
+     */
+    write(packet) {
+        if (!packet instanceof Packet)
+            throw TypeError("Connection write packet is not a Packet");
+
+        packet.source.external = this.#identity.address.address;
+
+        if (!this.#ready) {
+            this.#pending.push(packet);
+            return;
+        }
+
+        let buf = Buffer.alloc(22 + packet.data.length);
+        buf.writeUInt16BE(packet.data.length);
+        packet.source.buffer.copy(buf, 2);
+        packet.destination.buffer.copy(buf, 8);
+        packet.data.copy(buf, 14);
+
+        let bigi = BigInt(Math.floor(Date.now() / 1000));
+        buf.writeBigUInt64BE(bigi, 14 + packet.data.length); // Timestamp
+
+        const sig = crypto.sign("sha256", buf, this.#identity.key);
+
+        const sigBuf = Buffer.alloc(1);
+        sigBuf.writeUInt8(sig.length);
+
+        this.#socket.write(Buffer.concat([buf, sigBuf, sig], buf.length + sig.length + 1));
+    }
+
+    /**
+     * Reads a packet if available.
+     *
+     * @returns {Packet|null}
+     */
+    read() {
+        if (this.#packets.length > 0)
+            return this.#packets.shift();
+
+        return null;
+    }
+
+    /**
+     * @param {Buffer} data
+     */
+    #handshake(data) {
+        if (data[0] === 0 && data[1] === 0) {
+            if (data[2] === 0)
+                this.emit("error", Error("Peer does not support our version"));
+            else if (data[2] === 1)
+                this.emit("error", Error("Peer does not recognize our identity"));
+            else if (data[2] === 2)
+                this.emit("error", Error("Peer had an internal error"));
+            else
+                this.emit("error", Error("Peer had an unrecognized error"));
+
+            this.#socket.end();
+            return;
+        }
+
+        this.#ready = true;
+
+        let fthis = this;
+        this.#socket.on("data", (data) => {
+            let packet = fthis.#parse(data);
+
+            if (packet !== null) {
+                fthis.#packets.push(packet);
+                fthis.emit("data");
+            }
+        });
+
+        /*if (this.#pending.length === 0) {
+            let buf = Buffer.alloc(10, 0x00);
+
+            let bigi = BigInt(Math.floor(Date.now() / 1000));
+            buf.writeBigUInt64LE(bigi, 2); // Timestamp
+            const sig = crypto.sign("sha256", buf, this.#identity.key);
+
+            this.#socket.write(Buffer.concat([buf, sig], buf.length + 32));
+        } else {*/
+            for (let i = 0; i < this.#pending.length; i++)
+                this.write(this.#pending.shift());
+        //}
+
+        this.emit("ready");
+    }
+
+    /**
+     * @param {Buffer} data
+     * @return {Packet | null} data
+     */
+    #parse(data) {
+        let length = data.readUInt16BE();
+        let source = new Address(data.slice(2, 8));
+        let destination = new Address(data.slice(8, 14));
+        let dat = data.slice(14, length + 14);
+        let timestamp = data.readBigInt64BE(length + 14);
+        let sigLength = data.readUInt8(length + 22);
+        let sig = data.slice(length + 23, length + 23 + sigLength);
+        let key = KeyDatabase.getKey(source);
+
+        let now = BigInt(Math.floor(Date.now() / 1000));
+
+        if (now - timestamp > 60 || key === null)
+            return null;
+        else if (crypto.verify("sha256", dat, key, sig)) { // Decrypt error, instant abort.
+            this.#socket.end();
+            return null;
+        }
+
+        return new Packet(source, destination, dat);
     }
 }
 
--- Classes/Packet.js
+++ Classes/Packet.js
@@ -1,38 +1,51 @@
-"use strict";
+'use strict';
 
-/*
-	Packet:
-	
-	source:	GERTc source address
-	target:	GERTc target address
-	data:	NodeJS buffer storing the data
-*/
+const Address = require("./Address.js");
 
-function parseAddr(data) {
-	let first = (data[0] << 4) | (data[1] >> 4);
-	let second = ((data[1] & 0x0F) << 8) | data[2];
-	
-	return first.toString() + "." + second.toString();
+class Packet {
+    #source;
+    #destination;
+    #data;
+
+    /**
+     * Creates a new packet from two addresses and data.
+     *
+     * @param {Address} source
+     * @param {Address} destination
+     * @param {string | Buffer} data
+     */
+    constructor(source, destination, data) {
+        this.source = source;
+        this.destination = destination;
+        this.data = data;
+    }
+
+    get source() { return this.#source; }
+    set source(address) {
+        if (!address instanceof Address)
+            throw TypeError("Packet source is not an address");
+
+        this.#source = address;
+    }
+
+    get destination() { return this.#destination; }
+    set destination(address) {
+        if (!address instanceof Address)
+            throw TypeError("Packet destination is not an address");
+
+        this.#destination = address;
+    }
+
+    get data() { return this.#data; }
+    set data(data) {
+        if (typeof data === "string")
+            data = Buffer.from(data);
+
+        if (!data instanceof Buffer)
+            throw TypeError("Packet data is not a string or buffer");
+
+        this.#data = data;
+    }
 }
 
-function makePacket(data) {
-	if (data.length < 13) {
-		throw new Error("data");
-	}
-	
-	let source = parseAddr(data.slice(0, 3)) + ":" + parseAddr(data.slice(3, 6));
-	let target = parseAddr(data.slice(6, 9)) + ":" + parseAddr(data.slice(9, 12));
-	let len = data[12];
-	
-	if (data.length < 13 + len) {
-		throw new Error("data");
-	}
-	
-	return {
-		"source": source,
-		"target": target,
-		"data": data.slice(13, 13 + len)
-	}
-}
-
-module.exports = makePacket;
+module.exports = Packet;
